ebe6c0bcc3195b57ea331b74d1a4d87db02994b7
Philip Weiss <11620622+PrimeraTech@users.noreply.github.com>
2022-06-06 20:54:23 -0700

    Ongoing synchronization.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -25,26 +25,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         """Retrieve recipes for authenticated user."""
-        tags = self.request.query_params.get('tags')
-        ingredients = self.request.query_params.get('ingredients')
-        queryset = self.queryset
-        if tags:
-            tag_ids = self._params_to_ints(tags)
-            queryset = queryset.filter(tags__id__in=tag_ids)
-        if ingredients:
-            ingredient_ids = self._params_to_ints(ingredients)
-            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
-
-        return queryset.filter(
-            user=self.request.user
-        ).order_by('-id').distinct()
+        return self.queryset.filter(user=self.request.user).order_by('-id')
 
     def get_serializer_class(self):
         """Return the serializer class for request."""
         if self.action == 'list':
             return serializers.RecipeSerializer
-        elif self.action == 'upload_image':
-            return serializers.RecipeImageSerializer
 
         return self.serializer_class
 
@@ -63,21 +49,9 @@ class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
 
     def get_queryset(self):
         """Filter queryset to authenticated user."""
-        assigned_only = bool(
-            int(self.request.query_params.get('assigned_only', 0))
-        )
-        queryset = self.queryset
-        if assigned_only:
-            queryset = queryset.filter(recipe__isnull=False)
+        return self.queryset.filter(user=self.request.user).order_by('-name')
 
-        return queryset.filter(
-            user=self.request.user
-        ).order_by('-name').distinct()
-
-
-class TagViewSet(mixins.UpdateModelMixin,
-                 mixins.ListModelMixin,
-                 viewsets.GenericViewSet):
+class TagViewSet(BaseRecipeAttrViewSet):
     """Manage tags in the database."""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
